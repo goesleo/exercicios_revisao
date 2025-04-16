3b95712c719d6f70853cc24e091484a40efb4623
unknown <goesleo>
2025-04-15 21:34:03 -0300

    finalizado

---
+++ maiorNumero.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+   //Crie um programa Java que receba dois números e exiba o maior
+public class maiorNumero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite um numero: ");
+    int num1 = scanner.nextInt();
+    System.out.println("Digite outro numero: ");
+    int num2 = scanner.nextInt();
+
+    if(num1 > num2){
+
+        System.out.println( num1 +  " é maior que  " + num2);
+
+    }
+    else if(num1 < num2){
+        System.out.println(num2 + " é maior que  " + num1);
+
+    }
+    else{
+        System.out.println("os dois numeros são iguais ");
+    }
+
+
+
+
+
+
+
+
+
+
+    }
+}
